ca0f14e9eb806fa05ec0ec2b9545dbfe9d6c84f4
tradingtellus-web <tradingtellus@gmail.com>
2026-09-02 17:17:57 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '500mb' }));
+app.use(express.urlencoded({ limit: '500mb', extended: true }));
 
 const PORT = process.env.PORT || 3001;
 
